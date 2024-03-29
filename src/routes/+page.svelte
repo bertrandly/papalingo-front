575cec62d93a9d3e55d3faa71ce5c43d0a2d2953
bertrand <bertranddilly@yahoo.fr>
2024-03-29 15:49:58 +0100

    loading on start page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,13 @@
+<script>
+
+    import Loader from "../components/Loader.svelte";
+
+    let loading=false;
+    const changePage = async () => {
+        loading = true;
+    }
+</script>
+
 <div class="hero min-h-screen">
     <div class="hero-overlay bg-opacity-0"></div>
     <div class="hero-content text-center text-neutral-content">
@@ -6,7 +16,11 @@
                 <img src="logo_papalingo.png">
             </h1>
             <p class="mb-5">The only, custom-made, especially for Jeanne, English learning App</p>
-            <a href="/challenge" class="btn btn-primary">Get Started</a>
+            {#if !loading}
+                <a href="/challenge" class="btn btn-primary" on:click={changePage}>Get Started</a>
+            {:else}
+                <Loader/>
+            {/if}
         </div>
     </div>
 </div>
--- src/routes/challenge/+page.svelte
+++ src/routes/challenge/+page.svelte
@@ -4,6 +4,7 @@
     import {onMount} from "svelte";
     import {getNextChallenge, postChallengeParticipation} from "$lib/quizzApi.js";
     import {goto} from "$app/navigation";
+    import Loader from "../../components/Loader.svelte";
 
     /** @type {import('../../../../.svelte-kit/types/src/routes').PageData} */
     export let data;
@@ -17,6 +18,8 @@
         //load challenge if last challenge was before midnight
         if(data.challenge_participations[0].startedAt<now.toISOString()){
             nextChallenge = await getNextChallenge()
+        }else{
+            nextChallenge = false
         }
 
     });
@@ -29,15 +32,19 @@
 <div class="flex justify-center mt-3">
     <div class="w-1/3 text-center">
     {#if nextChallenge}
-        {#if nextChallenge.title}
-            <div class="">
-                <p><small>Today's challenge is:</small></p>
-                <h3>"{nextChallenge.title}"</h3>
-            </div>
+        {#if nextChallenge !== false}
+            {#if nextChallenge.title}
+                <div class="">
+                    <p><small>Today's challenge is:</small></p>
+                    <h3>"{nextChallenge.title}"</h3>
+                </div>
+            {/if}
+            <button on:click={startChallenge} class="btn btn-success btn-block ">Start</button>
+        {:else}
+            Job's done!
         {/if}
-        <button on:click={startChallenge} class="btn btn-success btn-block ">Start</button>
     {:else}
-        Job's done!
+        <Loader/>
     {/if}
     </div>
 </div>
